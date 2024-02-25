bde4e411452a447af72f031a70112816e52cb7d5
Ashutosh Kumar <ashutoshkumar.ash205@gmail.com>
2024-02-25 13:46:38 +0530

    Choose level page built

--- app/home/learn/addition/page.tsx
+++ app/home/learn/addition/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Button from '@/components/ui/Button'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className='bg-gradient-to-b from-White to-light-blue py-20 w-full min-h-screen'>
+      <h1 className='text-Black text-6xl text-center '> Choose Level</h1>
+      <Button 
+      href="/home/learn/addition/level"
+      customCSS="h-10 w-40 bg-orange mt-10"
+      text="Level 1"
+      />
     </div>
   )
 }
