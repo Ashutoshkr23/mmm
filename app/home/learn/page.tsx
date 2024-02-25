2de1cd82a7a1142be2bd329890feb73d1ee094d2
Ashutosh Kumar <ashutoshkumar.ash205@gmail.com>
2024-02-25 13:19:12 +0530

    Styling complete

--- app/home/learn/page.tsx
+++ app/home/learn/page.tsx
@@ -5,14 +5,14 @@ const page = () => {
     <div className="bg-gradient-to-b from-white to-Yellow min-h-screen py-20">
       <h1 className="text-Black text-center text-6xl">learn</h1>
       <div className="flex flex-wrap justify-around w-full max-w-[1180px] mx-auto mt-20 gap-10 ">
-        <Learn text="ADDITION" />
-        <Learn text="SUBTRACTION" />
-        <Learn text="MULTIPLICATION" />
-        <Learn text="DIVISION" />
-        <Learn text="TABLES" />
-        <Learn text="SQUARE" />
-        <Learn text="CUBE" />
-        <Learn text="RATION & PERCENTAGE" />
+        <Learn link="addition" text="ADDITION" />
+        <Learn link="subtraction" text="SUBTRACTION" />
+        <Learn link="multiplication" text="MULTIPLICATION" />
+        <Learn link="division" text="DIVISION" />
+        <Learn link="tables" text="TABLES" />
+        <Learn link="square" text="SQUARE" />
+        <Learn link="cube" text="CUBE" />
+        <Learn link="ratio" text="RATION & PERCENTAGE" />
       </div>
     </div>
   );
--- components/Cards/learn.tsx
+++ components/Cards/learn.tsx
@@ -1,13 +1,18 @@
+import Link from 'next/link';
 import React from 'react'
 
 interface LearnProps{
     text: string;
+    link: string;
 }
-const Learn = ({text} : LearnProps) => {
+const Learn = ({text , link} : LearnProps) => {
 
   return (
-    <div className='w-[500px] h-16 bg-Dark-blue rounded-xl flex items-center justify-center'>
-        <h1 className='text-4xl'>{text}</h1>
+    <div className='w-[500px] h-16 cursor-pointer bg-Dark-blue hover:bg-light-blue hover:border-Dark-blue hover:border-2 rounded-xl flex items-center justify-center'>
+        <Link href={`/home/learn/${link}`}>
+        <h1 className='text-4xl w-full h-full flex hover:text-Dark-blue justify-center items-center'>{text}</h1>
+        </Link>
+       
     </div>
   )
 }
